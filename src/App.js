7fa379211559f486b6a5e79a7c0363f435dad57c
sambitmohanty954 <sambitmohanty954@gmail.com>
2022-12-12 23:25:31 +0530

    improved darkmode for textutils

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import './App.css';
 // import AboutUs from './components/AboutUs';
 import NavBar from './components/NavBar';
 import TextForm from './components/TextForm';
+import React, { useState } from 'react'
  
 function App() {
+  const [mode, setmode] = useState("light")
+
+  const toggle = () => {
+    if(mode === "light"){
+      setmode("dark")
+      document.body.style.backgroundColor = "#042743";
+    } else {
+      setmode("light")
+      document.body.style.backgroundColor = "white";
+    }
+  }
   return (
     <>
-      <NavBar title="TextUtils" aboutText= "About Us"/>
+      <NavBar title="TextUtils" aboutText= "About Us" mode={mode} toggleMode = {toggle}/>
       <div className="container my-3">
-      <TextForm heading="Enter your text below"/>
+      <TextForm heading="Enter your text below" mode={mode}/>
       {/* <AboutUs /> */}
       </div>
     </>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -41,7 +41,7 @@ export default function TextForm(props) {
         }
         else {
             toogle.innerHTML = "Speak"
-            if(toogle.innerHTML == "Speak"){
+            if(toogle.innerHTML === "Speak"){
                 window.speechSynthesis.cancel()
             }
         }
@@ -49,17 +49,17 @@ export default function TextForm(props) {
 
     // Remove extra space newText= text.split(/[ ]+/)
         //setText(newText.join(" "))
-    const [text, setText] = useState("Type your text")
+    const [text, setText] = useState("")
     // text = "dhdhjd" //wrong way to update
     // setText("jdhjhdjhd") // correct way to update
 
     const textLength = text.split(" ").length
   return (
     <>
-    <div className='container'>
+    <div className='container' style={{color: props.mode === "dark" ? "white" : "black"}}>
         <h1>{props.heading}</h1>
         <div className="mb-3">
-            <textarea className="form-control"value={text} onChange={handleChangeEvent} id="exampleFormControlTextarea1" rows="8"></textarea>
+            <textarea className="form-control"value={text} onChange={handleChangeEvent} id="exampleFormControlTextarea1" style={{backgroundColor: props.mode === "dark" ? "grey":"white", color: props.mode === "dark" ? "white":"black"}} rows="8"></textarea>
         </div>
         <button className="btn btn-primary mx-1" onClick={handleClickEvent}>Convert to Uppercase</button>
         <button className="btn btn-primary mx-1" onClick={handleTitleCase}>Convert to Title Case</button>
@@ -68,12 +68,12 @@ export default function TextForm(props) {
         <button className="btn btn-primary mx-1" onClick={handleClearEvent}>Clear</button>
         <button className="btn btn-warning mx-1" onClick={speak} id='toggle'>Speak</button>
     </div>
-    <div className='container'>
+    <div className='container' style={{color: props.mode === "dark" ? "white" : "black"}}>
         <h2 className='my-3'>Your text summary</h2>
         <p>Your text have {textLength} words and {text.length} characters</p>
         <p>{Math.round(0.008 * textLength)} Minutes read</p>
         <h3>Preview</h3>
-        <p>{text}</p>
+        <p>{text.length > 0 ? text : "Enter something to preview"}</p>
     </div>
     </>
   )
